0285a070662a0ae9ef1845314d42408ce807a39c
Mikael Houdoux <mhoudoux@gmail.com>
2023-11-30 13:23:17 +0100

    Etape 9 - Le panier

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -11,13 +11,17 @@ class Cart
 
     }
 
+    /*
+     * add()
+     * Fonction permettant l'ajout d'un produit au panier
+     */
     public function add($product)
     {
         // Appeler la session CART de symfony
-        $cart = $this->requestStack->getSession()->get('cart');
+        $cart = $this->getCart();
 
         // Ajouter une qtity +1 à mon produit
-        if ($cart[$product->getId()]) {
+        if (isset($cart[$product->getId()])) {
             $cart[$product->getId()] = [
                 'object' => $product,
                 'qty' => $cart[$product->getId()]['qty'] + 1
@@ -33,6 +37,76 @@ class Cart
         $this->requestStack->getSession()->set('cart', $cart);
     }
 
+    /*
+     * decrease()
+     * Fonction permettant la suppression d'une quantity d'un produit au panier
+     */
+    public function decrease($id)
+    {
+        $cart = $this->getCart();
+
+        if ($cart[$id]['qty'] > 1) {
+            $cart[$id]['qty'] = $cart[$id]['qty'] - 1;
+        } else {
+            unset($cart[$id]);
+        }
+
+        $this->requestStack->getSession()->set('cart', $cart);
+    }
+
+    /*
+     * fullQuantity()
+     * Fonction retournant le nombre total de produit au panier
+     */
+    public function fullQuantity()
+    {
+        $cart = $this->getCart();
+        $quantity = 0;
+
+        if (!isset($cart)) {
+            return $quantity;
+        }
+
+        foreach ($cart as $product) {
+            $quantity = $quantity + $product['qty'];
+        }
+
+        return $quantity;
+    }
+
+    /*
+     * getTotalWt()
+     * Fonction retournant le prix total des produits au panier
+     */
+    public function getTotalWt()
+    {
+        $cart = $this->getCart();
+        $price = 0;
+
+        if (!isset($cart)) {
+            return $price;
+        }
+
+        foreach ($cart as $product) {
+            $price = $price + ($product['object']->getPriceWt() * $product['qty']);
+        }
+
+        return $price;
+    }
+
+    /*
+     * remove()
+     * Fonction permettant de supprimer totalement le panier
+     */
+    public function remove()
+    {
+        return $this->requestStack->getSession()->remove('cart');
+    }
+
+    /*
+     * getCart()
+     * Fonction retournant le panier
+     */
     public function getCart()
     {
         return $this->requestStack->getSession()->get('cart');
--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Classe\Cart;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,12 +15,13 @@ class CartController extends AbstractController
     public function index(Cart $cart): Response
     {
         return $this->render('cart/index.html.twig', [
-            'cart' => $cart->getCart()
+            'cart' => $cart->getCart(),
+            'totalWt' => $cart->getTotalWt()
         ]);
     }
 
     #[Route('/cart/add/{id}', name: 'app_cart_add')]
-    public function add($id, Cart $cart, ProductRepository $productRepository): Response
+    public function add($id, Cart $cart, ProductRepository $productRepository, Request $request): Response
     {
         $product = $productRepository->findOneById($id);
 
@@ -30,8 +32,28 @@ class CartController extends AbstractController
             "Produit correctement ajouté à votre panier."
         );
 
-        return $this->redirectToRoute('app_product', [
-            'slug' => $product->getSlug()
-        ]);
+        return $this->redirect($request->headers->get('referer'));
+    }
+
+
+    #[Route('/cart/decrease/{id}', name: 'app_cart_decrease')]
+    public function decrease($id, Cart $cart): Response
+    {
+        $cart->decrease($id);
+
+        $this->addFlash(
+            'success',
+            "Produit correctement supprimée de votre panier."
+        );
+
+        return $this->redirectToRoute('app_cart');
+    }
+
+    #[Route('/cart/remove', name: 'app_cart_remove')]
+    public function remove(Cart $cart): Response
+    {
+        $cart->remove();
+
+        return $this->redirectToRoute('app_home');
     }
 }
