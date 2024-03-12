82a4f86c019404071d15c7d61e5d9590d81c5e09
Mikael Houdoux <mhoudoux@gmail.com>
2024-03-12 14:33:59 +0100

    Commit pour la production vers Hostinger

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Classe\Mail;
 use App\Entity\User;
 use App\Form\RegisterUserType;
 use Doctrine\ORM\EntityManagerInterface;
@@ -29,6 +30,13 @@ class RegisterController extends AbstractController
                 'success',
                 "Votre compte est correctement créé, veuillez vous connecter."
             );
+
+            // Envoie d'un email de confirmation d'inscription
+            $mail = new Mail();
+            $vars = [
+                'firstname' => $user->getFirstname(),
+            ];
+            $mail->send($user->getEmail(), $user->getFirstname().' '.$user->getLastname(), "Bienvenue sur La Boutique Française", "welcome.html", $vars);
 
             return $this->redirectToRoute('app_login');
         }
